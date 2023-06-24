8e8093edf71f28bac7689a5b850a65eaaf26739e
Adam Englishby <adam@englishby.ie>
2023-06-24 19:24:55 +0100

    feat(menu): add functional main menu

---
+++ Assets/GGJ21/Scripts/MainMenu/AnyButtonDetector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.LowLevel;
+
+public class AnyButtonDetector : MonoBehaviour
+{
+
+    private InputAction _action;
+    
+    private void Start()
+    {
+        InputSystem.onEvent +=
+            (eventPtr, device) =>
+            {
+                if (!eventPtr.IsA<StateEvent>() && !eventPtr.IsA<DeltaStateEvent>())
+                    return;
+                var controls = device.allControls;
+                var buttonPressPoint = InputSystem.settings.defaultButtonPressPoint;
+                foreach (var t in controls)
+                {
+                    var control = t as ButtonControl;
+                    if (control == null || control.synthetic || control.noisy)
+                        continue;
+                    if (!control.ReadValueFromEvent(eventPtr, out var value) || !(value >= buttonPressPoint)) continue;
+                    Debug.Log("Pressed");
+                    break;
+                }
+            };
+    }
+}
---
+++ Assets/GGJ21/Scripts/MainMenu/TyperwriterPacing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using Febucci.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.SceneManagement;
+
+public class TyperwriterPacing : MonoBehaviour
+{
+    public TypewriterByCharacter line1, line2, tagline, pressAnyButton;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1);
+        line1.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2.1f);
+        line2.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        tagline.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        pressAnyButton.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+
+        InputSystem.onEvent += InputEvent;
+    }
+
+    private static void InputEvent(InputEventPtr eventPtr, InputDevice device)
+    {
+        if (!eventPtr.IsA<StateEvent>() && !eventPtr.IsA<DeltaStateEvent>())
+        {
+            return;
+        }
+
+        var controls = device.allControls;
+        var buttonPressPoint = InputSystem.settings.defaultButtonPressPoint;
+        foreach (var t in controls)
+        {
+            var control = t as ButtonControl;
+            if (control == null || control.synthetic || control.noisy)
+                continue;
+            if (!control.ReadValueFromEvent(eventPtr, out var value) || !(value >= buttonPressPoint)) continue;
+            Debug.Log("Pressed");
+            InputSystem.onEvent -= InputEvent;
+            SceneManager.LoadScene(sceneName: "SampleScene");
+            break;
+        }
+    }
+}
