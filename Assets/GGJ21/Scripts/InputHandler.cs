10bc52f7a6574cd9065bb0cf7beb4bbd11f9631b
Adam Englishby <adam@englishby.ie>
2023-06-24 13:18:02 +0100

    feat(scene): add placeholder room environment

--- Assets/GGJ21/Scripts/InputHandler.cs
+++ Assets/GGJ21/Scripts/InputHandler.cs
@@ -3,13 +3,20 @@ using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
-    
-    
+
+    [SerializeField] private CharacterController controller;
+    private Vector2 _moveDirection;
+
+    public float speed = 1;
     
     public void OnMovement(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
-        
+        _moveDirection = value.Get<Vector2>();
     }
-    
+
+    public void FixedUpdate()
+    {
+        Debug.Log(_moveDirection);
+        controller.Move(new Vector3(_moveDirection.x, 0, _moveDirection.y) * Time.fixedDeltaTime * speed);
+    }
 }
