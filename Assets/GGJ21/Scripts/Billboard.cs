8f8cf31e5701450f60b13e439ac750a0d1602ad4
Adam Englishby <adam@englishby.ie>
2023-06-24 10:31:46 +0100

    feat(project): add new input system

--- Assets/GGJ21/Scripts/Billboard.cs
+++ Assets/GGJ21/Scripts/Billboard.cs
@@ -5,8 +5,6 @@ public class Billboard : MonoBehaviour
     // cache these to avoid extern calls in Update()
     private Transform _transform, _cameraTransform;
 
-    private float yAngle;
-
     private void Awake()
     {
         _transform = transform;
@@ -24,8 +22,5 @@ public class Billboard : MonoBehaviour
         var rotation = _transform.rotation;
         rotation = Quaternion.Euler(rotation.eulerAngles.x, _cameraTransform.rotation.eulerAngles.y - 180, rotation.eulerAngles.z);
         _transform.rotation = rotation;
-            
-        yAngle = Vector3.SignedAngle(_transform.position, _cameraTransform.transform.position, Vector3.up);
-        Debug.Log(yAngle);
     }
 }
---
+++ Assets/GGJ21/Scripts/InputHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour, InputActions.IActionMapActions
+{
+    public void OnSpace(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            Debug.Log("Action was started");
+        else if (context.performed)
+            Debug.Log("Action was performed");
+        else if (context.canceled)
+            Debug.Log("Action was cancelled");
+    }
+}
