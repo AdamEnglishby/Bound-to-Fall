dffa237c0d5feabe4474bdeaea92fe5fb4eae1f8
Adam Englishby <adam@englishby.ie>
2023-06-24 11:29:32 +0100

    fix(input): remove dead code

--- Assets/GGJ21/Scripts/InputHandler.cs
+++ Assets/GGJ21/Scripts/InputHandler.cs
@@ -3,16 +3,6 @@ using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
-    public void OnSpace(InputAction.CallbackContext context)
-    {
-        if (context.started)
-            Debug.Log("Action was started");
-        else if (context.performed)
-            Debug.Log("Action was performed");
-        else if (context.canceled)
-            Debug.Log("Action was cancelled");
-    }
-
     public void OnMovement(InputValue value)
     {
         Debug.Log(value.Get<Vector2>());
